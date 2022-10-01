6c157762d92ef6c107e832a391bb6fb9cc5ad825
pamela <p.tavares93270@gmail.com>
2022-10-01 22:37:12 +0200

    ecoute evenements

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -49,7 +49,8 @@ class ConferenceController extends AbstractController
     #[Route('/conference/{id}', name: 'conference')]
     // public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
     // public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    // public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
 
         $offset = max(0, $request->query->getInt('offset', 0));
@@ -57,6 +58,7 @@ class ConferenceController extends AbstractController
 
         // return new Response($twig->render('conference/show.html.twig', [
         return new Response($this->twig->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
             // 'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
             'comments' => $paginator,
